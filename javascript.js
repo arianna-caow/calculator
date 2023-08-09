82d11fff2b064abba35a84b3609c228a4e0b1636
Arianna <66382962+arianna-caow@users.noreply.github.com>
2023-08-09 18:00:36 -0400

    created basic css layout

--- javascript.js
+++ javascript.js
@@ -11,7 +11,52 @@ function divide(a,b){
     return a/b;
 }
 
-console.log(add(1,2));
-console.log(subtract(1,2));
-console.log(multiply(1,2));
-console.log(divide(1,2));
+function operate (op, n1, n2){
+    if (op==="+"){
+        return add(n1,n2);
+    } else if (op==="-"){
+        return subtract(n1,n2);
+    } else if (op==="*"){
+        return multiply(n1,n2);
+    } else if (op==="/"){
+        return divide(n1,n2);
+    }
+}
+// console.log(operate("*",1,2))
+// console.log(operate("-",1,2))
+// console.log(operate("+",1,2))
+// console.log(operate("/",1,2))
+
+function createButtons(){
+    const outerContainer = document.querySelector("#numbers");
+    for (let i = 7; i >0; i-=3){
+        const container = document.createElement("div");
+        container.classList.add("row");
+        for (let j = 0; j < 3; j++) {
+            const numButton = document.createElement("button");
+            numButton.innerText=(i+j);
+            numButton.classList.add("button");
+            container.appendChild(numButton);
+        }
+        outerContainer.appendChild(container);
+    }
+    const container = document.createElement("div");
+    container.classList.add("row");
+    const zero = document.createElement("button");
+    zero.innerText=0;
+    zero.classList.add("button");
+    container.appendChild(zero);
+    const period = document.createElement("button");
+    period.innerText=".";
+    period.classList.add("button");
+    container.appendChild(period);
+    const equal = document.createElement("button");
+    equal.innerText="=";
+    equal.classList.add("button");
+    container.appendChild(equal);
+    outerContainer.appendChild(container);
+}
+
+createButtons();
+
+
