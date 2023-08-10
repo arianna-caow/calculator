b7270474448a1c2891d24213e05a97f33d9c06fe
Arianna <66382962+arianna-caow@users.noreply.github.com>
2023-08-10 12:12:24 -0400

    made buttons display on screen

--- javascript.js
+++ javascript.js
@@ -37,26 +37,39 @@ function createButtons(){
         b.classList.add("button");
         upper.appendChild(b);
     }
-
-    const outerContainer = document.querySelector("#numbers");
-    for (let i = 7; i >0; i-=3){
+    const op = ["÷","×","–","+"];
+    const outerContainer = document.querySelector("#allButtons");
+    let count = 0;
+    for (let i = 7; i > 0; i-=3){
         const container = document.createElement("div");
         container.classList.add("row");
         for (let j = 0; j < 3; j++) {
             const numButton = document.createElement("button");
             numButton.innerText=(i+j);
             numButton.classList.add("button");
+            numButton.classList.add("number");
             container.appendChild(numButton);
         }
+        const operator = document.createElement("button");
+        operator.innerText=(op[count]);
+        operator.classList.add("button");
+        operator.classList.add("operator");
+        operator.style.backgroundColor="#B7C9E2";
+        container.appendChild(operator);
         outerContainer.appendChild(container);
+        count++
     }
-    const lowArr = ["0",".","="];
+    const lowArr = ["0",".","=","+"];
     const container = document.createElement("div");
     container.classList.add("row");
     for(const char of lowArr){
         const b = document.createElement("button");
         b.innerText=char;
         b.classList.add("button");
+        if (char==="+" || char==="="){
+            b.classList.add("operator");
+            b.style.backgroundColor="#B7C9E2";
+        }
         container.appendChild(b);
     }
     outerContainer.appendChild(container);
@@ -64,4 +77,17 @@ function createButtons(){
 
 createButtons();
 
-
+const display = document.querySelector("#screen");
+const equation = document.querySelector('#equation');
+const numButtons = document.querySelectorAll(".number");
+const allButtons = document.querySelectorAll(".button");
+numButtons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        display.textContent=button.textContent;
+    })
+})
+allButtons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        equation.textContent+=button.textContent;
+    })
+})
